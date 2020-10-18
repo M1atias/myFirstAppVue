9a25f3c8403ec9bc0fcaf50081b82ad1eeb914ea
MatiasMolina <maty.f.molina4a@gmail.com>
2020-10-17 23:10:48 -0300

    add: Comienzo con VUE

---
+++ myFirstAppVue/src/components/test.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+<div v-text="title"></div>
+<div v-if="!showName">
+<p>No puedo mostrar nombre</p>
+</div>
+<div v-else>
+    <p>{{user.firstName}}</p>
+</div>
+<br>
+<ul>
+    <li v-for= "task in tasks">
+        {{task.title}}
+    </li>
+</ul>
+</div>
+</template>
+
+
+<script>
+export default{
+    data() {
+        return {
+            title: 'Hello Vuejs',
+            user:{
+                firstName:'test',
+                lastName: 'test21'
+            },
+            myHtml: '<h4>hola vue</h4>',
+            showName: true,
+            tasks: [
+                {title: 'eat'},
+                {title: 'dinner'},
+                {title: 'play'}
+            ]
+        }
+    }
+}
+</script>
+
+<style media="screen">
+
+</style>
